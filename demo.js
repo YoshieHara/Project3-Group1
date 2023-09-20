d09760bf391aba4fa41335e633d3a95d8caeecd7
Yoshie Hara <133894305+YoshieHara@users.noreply.github.com>
2023-09-20 21:52:15 +1000

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,25 +2,27 @@
 const apiUrl = 'http://localhost:5000/api/v1.0/CrimesInVictoria';
 const apiUrl2 = 'http://localhost:5000/api/v1.0/DrugsOffencesVictoria';
 
-// Define global variables
+// Define variables
 let data;
 let data2;
 let lineChart;
 let lineChart2;
 let pieChart;
 let barChart;
+
+//these variables used to obtain selected values from HTML form elements.
 let selectOffenceDivision = document.getElementById('selectOffenceDivision');
 let selectYearPie = document.getElementById('selectYearPie');
 let selectYearBar = document.getElementById('selectYearBar');
 let selectOffenceGroup = document.getElementById('selectOffenceGroup');
-let selectOffenceGroup2 = document.getElementById('selectOffenceGroup2'); // Define selectOffenceGroup2
+let selectOffenceGroup2 = document.getElementById('selectOffenceGroup2'); 
 
 
 
 
+// Function to create and update the linechart, "if" for Total for each year and "else" for each division for each year
 function createLineChart(selectedOffenceDivision) {
     if (selectedOffenceDivision === "Total (all divisions)") {
-        // Calculate the sum of all offence divisions for each year
         const yearlyData = {};
         data.forEach(d => {
             const year = d["Year"];
@@ -31,45 +33,47 @@ function createLineChart(selectedOffenceDivision) {
             yearlyData[year] += count;
         });
 
+        //do the Data Extraction After iterating through all the data entries,
         const years = Object.keys(yearlyData);
         const counts = Object.values(yearlyData);
 
-        // Create a line chart with the sum data
+        // and Create a line chart with the sum data, destroy if any exist one from the testing
         const ctxLineChart = document.getElementById('lineChartCanvas').getContext('2d');
         if (lineChart) {
             lineChart.destroy();
         }
         lineChart = new Chart(ctxLineChart, {
-            type: 'line',
+            type: 'line', // set the chart type
             data: {
-                labels: years,
+                labels: years, //xaxis we use years
                 datasets: [{
-                    label: 'OffenceCount',
-                    data: counts,
-                    borderColor: 'steelblue',
-                    backgroundColor: 'rgba(70, 130, 180, 0.3)',
-                    borderWidth: 1
+                    label: 'Offence Count', //for the label of the line
+                    data: counts, //ylabel data
+                    borderColor: 'steelblue', // line color
+                    backgroundColor: 'rgba(70, 130, 180, 0.3)', //dot color
+                    borderWidth: 1 //line tickness
                 }]
             },
             options: {
                 scales: {
                     x: {
                         title: {
-                            display: true,
-                            text: 'Year'
+                            display: true, // to display xaxis
+                            text: 'Year' //name of xaxis
                         }
                     },
                     y: {
                         title: {
                             display: true,
-                            text: 'OffenceCount'
+                            text: 'Offence Count'
                         }
                     }
                 }
             }
         });
+
+    //filter the data to get the details based on the selected OffenceDivision 
     } else {
-        // Filter data based on the selected OffenceDivision
         const filteredData = data.filter(d => d["OffenceDivision"] === selectedOffenceDivision);
 
         // Extract unique Years and corresponding OffenceCount
@@ -94,12 +98,12 @@ function createLineChart(selectedOffenceDivision) {
         lineChart = new Chart(ctxLineChart, {
             type: 'line',
             data: {
-                labels: years,
+                labels: years, //xaxis, const by two section before, 
                 datasets: [{
-                    label: 'OffenceCount',
-                    data: counts,
+                    label: 'Offence Count',  //yaxis
+                    data: counts, // const by two section before
                     borderColor: 'steelblue',
-                    backgroundColor: 'rgba(70, 130, 180, 0.3)',
+                    backgroundColor: 'rgba(70, 130, 180, 0.3)', //dot color as steelblue
                     borderWidth: 1
                 }]
             },
@@ -114,7 +118,7 @@ function createLineChart(selectedOffenceDivision) {
                     y: {
                         title: {
                             display: true,
-                            text: 'OffenceCount'
+                            text: 'Offence Count'
                         }
                     }
                 }
@@ -122,6 +126,7 @@ function createLineChart(selectedOffenceDivision) {
         });
     }
 }
+
 
 
 
@@ -187,204 +192,21 @@ function createPieChart(selectedYear) {
     });
 }
 
-function createLineChart2(selectedOffenceGroup) {
-    if (selectedOffenceGroup === "Total (all groups)") {
-        // Calculate the sum of all offence groups for each year in URL2
-        const yearlyData2 = {};
-        data2.forEach(d => {
-            const year = d["Year"];
-            const count = d["OffenceCount"];
-            if (!yearlyData2[year]) {
-                yearlyData2[year] = 0;
-            }
-            yearlyData2[year] += count;
-        });
-
-        const years2 = Object.keys(yearlyData2);
-        const counts2 = Object.values(yearlyData2);
-
-        // Create a line chart for URL2 with the sum data
-        const ctxLineChart2 = document.getElementById('lineChartCanvas2').getContext('2d');
-        if (lineChart2) {
-            lineChart2.destroy();
-        }
-
-        // Customize chart colors and labels for URL2
-        const borderColor = 'steelblue';
-        const backgroundColor = 'rgba(70, 130, 180, 0.3)';
-        const xTitle = 'Year';
-        const yTitle = 'OffenceCount';
-
-        lineChart2 = new Chart(ctxLineChart2, {
-            type: 'line',
-            data: {
-                labels: years2,
-                datasets: [{
-                    label: 'OffenceCount',
-                    data: counts2,
-                    borderColor: 'green',
-                    backgroundColor: 'rgba(0, 128, 0, 0.3)', // Change the background color for URL2
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: xTitle
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: yTitle
-                        }
-                    }
-                }
-            }
-        });
-    } else {
-        // Filter data from URL2 based on the selected OffenceGroup
-        const filteredData2 = data2.filter(d => d["OffenceGroup"] === selectedOffenceGroup);
-
-        // Extract unique Years and corresponding OffenceCount for URL2
-        const yearlyData2 = {};
-        filteredData2.forEach(d => {
-            const year = d["Year"];
-            const count = d["OffenceCount"];
-            if (!yearlyData2[year]) {
-                yearlyData2[year] = 0;
-            }
-            yearlyData2[year] += count;
-        });
-
-        const years2 = Object.keys(yearlyData2);
-        const counts2 = Object.values(yearlyData2);
-
-        // Create a line chart for URL2 with the selected offence group data
-        const ctxLineChart2 = document.getElementById('lineChartCanvas2').getContext('2d');
-        if (lineChart2) {
-            lineChart2.destroy();
-        }
-
-        // Customize chart colors and labels for URL2
-        const borderColor = 'steelblue';
-        const backgroundColor = 'rgba(70, 130, 180, 0.3)';
-        const xTitle = 'Year';
-        const yTitle = 'OffenceCount';
-
-        lineChart2 = new Chart(ctxLineChart2, {
-            type: 'line',
-            data: {
-                labels: years2,
-                datasets: [{
-                    label: 'OffenceCount',
-                    data: counts2,
-                    borderColor: 'green',
-                    backgroundColor: 'rgba(0, 128, 0, 0.3)', // Change the background color for URL2
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: xTitle
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: yTitle
-                        }
-                    }
-                }
-            }
-        });
-    }
-}
-
-
-
-// Updated createBarChart function
-function createBarChart(selectedYear) {
-    // Filter data based on the selected year
-    const filteredData = data2.filter(d => d["Year"] == selectedYear);
-
-    // Extract unique Offence Groups for the bar chart
-    const offenceGroups = [...new Set(filteredData.map(d => d["OffenceGroup"]))];
-
-    // Extract counts for each Offence Group
-    const counts = offenceGroups.map(group => {
-        return filteredData.reduce((acc, d) => {
-            if (d["OffenceGroup"] === group) {
-                return acc + d["OffenceCount"];
-            }
-            return acc;
-        }, 0);
-    });
-
-    // Create a bar chart
-    const ctxBarChart = document.getElementById('barChartCanvas').getContext('2d');
-    if (barChart) {
-        barChart.destroy();
-    }
-    barChart = new Chart(ctxBarChart, {
-        type: 'bar',
-        data: {
-            labels: offenceGroups,
-            datasets: [{
-                label: 'Offence Count',
-                data: counts,
-                backgroundColor: 'rgba(70, 130, 180, 0.7)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Offence Group'
-                    },
-                    ticks: {
-                        autoSkip: false, // Disable automatic label skipping
-                        maxRotation: 15,   // Rotate labels to 0 degrees (vertical)
-                        minRotation: 15    // Rotate labels to 0 degrees (vertical)
-                    }
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Offence Count'
-                    }
-                }
-            },
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
-        }
-    });
-
-    // Adjust bar and category percentages for visibility
-    barChart.options.plugins.barThickness = 'flex';
-    barChart.options.plugins.categoryPercentage = 1.0;
-}
 
 
 // Fetch data from the first API URL and populate dropdowns
 fetch(apiUrl)
     .then(response => response.json())
     .then(jsonData => {
-        // Store the data globally
+        // Store the data
         data = jsonData;
 
-        // Extract unique OffenceDivisions for the dropdown
+        // Extract unique OffenceDivisions for the dropdown, 
+        // use  "..." to convert the Set into an array, otherwise not showing in the dropdown correctly
         const uniqueOffenceDivisions = [...new Set(data.map(d => d["OffenceDivision"]))];
+
+        // Clear the selectOffenceDivision dropdown before populating it
+        selectOffenceDivision.innerHTML = "";
 
         // Add "Total (all divisions)" as the first option in the dropdown
         const totalOption = document.createElement("option");
@@ -467,6 +289,183 @@ fetch(apiUrl)
     .catch(error => {
         console.error("Error loading data from the first API URL:", error);
     });
+
+
+
+
+// From here use URL2    
+// Function to create and update the linechart2, do the same as the first line chart
+function createLineChart2(selectedOffenceGroup) {
+    if (selectedOffenceGroup === "Total (all groups)") {
+        const yearlyData2 = {};
+        data2.forEach(d => {
+            const year = d["Year"];
+            const count = d["OffenceCount"];
+            if (!yearlyData2[year]) {
+                yearlyData2[year] = 0;
+            }
+            yearlyData2[year] += count;
+        });
+
+        const years2 = Object.keys(yearlyData2);
+        const counts2 = Object.values(yearlyData2);
+
+        // Create a line chart for URL2 with the sum data
+        const ctxLineChart2 = document.getElementById('lineChartCanvas2').getContext('2d');
+        if (lineChart2) {
+            lineChart2.destroy();
+        }
+
+        lineChart2 = new Chart(ctxLineChart2, {
+            type: 'line',
+            data: {
+                labels: years2,
+                datasets: [{
+                    label: 'Offence Count',
+                    data: counts2,
+                    borderColor: 'green',
+                    backgroundColor: 'rgba(0, 128, 0, 0.3)', // Change the background color for URL2
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Year'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Offence Count'
+                        }
+                    }
+                }
+            }
+        });
+    } else {
+        // Filter data from URL2 based on the selected OffenceGroup
+        const filteredData2 = data2.filter(d => d["OffenceGroup"] === selectedOffenceGroup);
+
+        // Extract unique Years and corresponding OffenceCount for URL2
+        const yearlyData2 = {};
+        filteredData2.forEach(d => {
+            const year = d["Year"];
+            const count = d["OffenceCount"];
+            if (!yearlyData2[year]) {
+                yearlyData2[year] = 0;
+            }
+            yearlyData2[year] += count;
+        });
+
+        const years2 = Object.keys(yearlyData2);
+        const counts2 = Object.values(yearlyData2);
+
+        // Create a line chart for URL2 with the selected offence group data
+        const ctxLineChart2 = document.getElementById('lineChartCanvas2').getContext('2d');
+        if (lineChart2) {
+            lineChart2.destroy();
+        }
+
+        lineChart2 = new Chart(ctxLineChart2, {
+            type: 'line',
+            data: {
+                labels: years2,
+                datasets: [{
+                    label: 'OffenceCount',
+                    data: counts2,
+                    borderColor: 'green',
+                    backgroundColor: 'rgba(0, 128, 0, 0.3)', // Change the background color for URL2
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Year'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'Offence Count'
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+
+
+
+// Updated createBarChart function
+function createBarChart(selectedYear) {
+    // Filter data based on the selected year
+    const filteredData = data2.filter(d => d["Year"] == selectedYear);
+
+    // Extract unique Offence Groups for the bar chart
+    const offenceGroups = [...new Set(filteredData.map(d => d["OffenceGroup"]))];
+
+    // Extract counts for each Offence Group
+    const counts = offenceGroups.map(group => {
+        return filteredData.reduce((acc, d) => {
+            if (d["OffenceGroup"] === group) {
+                return acc + d["OffenceCount"];
+            }
+            return acc;
+        }, 0);
+    });
+
+    // Create a bar chart
+    const ctxBarChart = document.getElementById('barChartCanvas').getContext('2d');
+    if (barChart) {
+        barChart.destroy();
+    }
+    barChart = new Chart(ctxBarChart, {
+        type: 'bar',
+        data: {
+            labels: offenceGroups,
+            datasets: [{
+                label: 'Offence Count',
+                data: counts,
+                backgroundColor: 'rgba(70, 130, 180, 0.7)',
+                borderWidth: 1,
+                barThickness:20
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Offence Group'
+                    },
+                    ticks: {
+                        autoSkip: false, // Disable automatic label skipping
+                        maxRotation: 15,   // Rotate labels to 0 degrees (vertical)
+                        minRotation: 15    // Rotate labels to 0 degrees (vertical)
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Offence Count'
+                    }
+                }
+            },
+        }
+    });
+}
+
+
+
+
+
 
 // Fetch data from URL2 and populate dropdowns
 fetch(apiUrl2)
@@ -555,9 +554,8 @@ fetch(apiUrl2)
             createBarChart(selectedYear);
         });
 
-        // Initialize the line chart with default values for URL2
+        // Initialize the line chart and bar chart with default values
         createLineChart2("Total (all groups)");
-        // Set the default year for the bar chart
         createBarChart(uniqueYears2[0]);
     })
     .catch(error => {
